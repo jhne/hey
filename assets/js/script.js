da0449a78fbaf8f8f6d3ab5fe9098a6900beebd5
John Elgenstierna <jhne@squareup.com>
2016-04-01 16:56:36 -0400

    Pretty

--- assets/js/script.js
+++ assets/js/script.js
@@ -13,19 +13,33 @@ $(document).ready(function(){
     // Sending a msg
     $('form').submit(function(){
         var fieldVal = $('#m').val();
-        socket.emit('chat message', ''+ name+': '+ fieldVal +'');
-        $('#m').val('');
-        $('#messages').append($('<li class="msg you"><p>'+ fieldVal +'</p></li>'));
-        return false;
+        if (fieldVal === "") {
+            // nothing
+        } else {
+            socket.emit('chat message', ''+ name+': '+ fieldVal +'');
+            $('#m').val('');
+            $('#messages').append($('<li class="msg you animated fadeInUp"><p>'+ fieldVal +'</p></li>'));
+            return false;
+        }
+        $('#m').focus();
     });
 
     // Displaying a msg
     socket.on('chat message', function(msg){
-        $('#messages').append($('<li class="msg"><p>'+msg+'</p></li>'));
+        $('#messages').append($('<li class="msg animated fadeInUp"><p>'+msg+'</p></li>'));
     });
 
     // Connection message
     socket.on('connectionNotification', function(name){
-        $('#messages').append($('<li class="system"><p>'+name+' connected</p></li>'));
+        $('#messages').append($('<li class="system animated fadeInUp"><p>'+name+' connected</p></li>'));
     });
+
+    /*
+    
+    We probably don't want to send the nickname and message as
+    a whole string in the future since we're locked to displaying
+    it in a certain way if we do.
+
+
+    */
 });
